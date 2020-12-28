66db96b61dd763219d30383e12348bb3c8d75985
domgunewardena <66011295+domgunewardena@users.noreply.github.com>
2020-12-28 08:34:07 +0000

    Create secrets.py

---
+++ data/database/secrets.py
@@ -0,0 +1,4 @@
+postgres_host = 'ec2-3-251-0-202.eu-west-1.compute.amazonaws.com'
+postgres_database = 'dfqduhvokqgmrb'
+postgres_user = 'zihnwwiixbqfwq'
+postgres_password = '83a006318f7f6b7877883c88a16c8ef351faa58f50897bd57103db26c05a58f7'
