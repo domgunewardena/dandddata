fca7373ad6af60ce866b2ddc8adc8c84483acb8d
domgunewardena <66011295+domgunewardena@users.noreply.github.com>
2021-01-01 07:51:40 +0000

    Update navbar.py

--- display/navbar.py
+++ display/navbar.py
@@ -2,61 +2,6 @@ import dash
 import dash_bootstrap_components as dbc
 
 def Navbar():
-    navbar = dbc.NavbarSimple(
-        children=[
-            dbc.NavItem(
-                dbc.NavLink(
-                    "Daily Sales", 
-                    href="/daily-sales")
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    "WTD Sales", 
-                    href="/wtd-sales")
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    "MTD Sales", 
-                    href="/mtd-sales")
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    "Weekly Sales", 
-                    href="/weekly-sales")
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    "Monthly Sales", 
-                    href="/monthly-sales")
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    "Cover Tracker", 
-                    href="/tracker")
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    "Daily Pickup", 
-                    href="/pickup")
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    "Future Bookings", 
-                    href="/future")
-            ),
-            dbc.NavItem(
-                dbc.NavLink(
-                    "Booking Trends", 
-                    href="/trends")
-            )
-        ],
-        brand="D&D Data",
-        brand_href="/",
-        sticky="top")
-    
-    return navbar
-
-def Navbar2():
     
     navbar = dbc.NavbarSimple(
         children=[
@@ -126,4 +71,4 @@ def Navbar2():
     
     return navbar
 
-nav = Navbar2()
+nav = Navbar()
