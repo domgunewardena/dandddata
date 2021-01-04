0f0eb932ae121c84ba1ae26a8afe9a30d2aa78a7
domgunewardena <66011295+domgunewardena@users.noreply.github.com>
2021-01-04 08:13:56 +0000

    Update data.py

--- data/data.py
+++ data/data.py
@@ -6,90 +6,28 @@ from authentication.authentication import auth
 from authentication.users import user_restaurants, bookings_user_restaurants
 
 from data.date_bounds import bound_filtering, daily_bounds, wtd_bounds, mtd_bounds, weekly_bounds, monthly_bounds
+from data.skeleton_df import generate_skeleton_df
 
-# from data.database.postgresql_tables import revenue, covers, tracker, pickup, future, trends
+from data.database.postgresql_tables import revenue, covers, tracker, pickup, future, trends
 
-rev_df = pd.read_csv("data/App Revenue.csv")
-cov_df = pd.read_csv("data/App Covers.csv")
+# rev_df = pd.read_csv("data/App Revenue.csv")
+# cov_df = pd.read_csv("data/App Covers.csv")
 
-tracker_df = pd.read_csv('data/Tracker.csv').iloc[:,1:]
-pickup_df = pd.read_csv('data/Pickup.csv').iloc[:,1:]
+# tracker_df = pd.read_csv('data/Tracker.csv').iloc[:,1:]
+# pickup_df = pd.read_csv('data/Pickup.csv').iloc[:,1:]
 
-future_df = pd.read_csv("data/Future Bookings.csv")
+# future_df = pd.read_csv("data/Future Bookings.csv")
+# future_df['weekday'] = pd.to_datetime(future_df['visit_day']).dt.weekday_name
+
+# trends_df = pd.read_csv('data/Booking Trends.csv')
+
+tracker_df = tracker.dataframe
+pickup_df = pickup.dataframe
+trends_df = trends.dataframe
+future_df = future.dataframe
 future_df['weekday'] = pd.to_datetime(future_df['visit_day']).dt.weekday_name
-
-trends_df = pd.read_csv('data/Booking Trends.csv')
-
-def generate_skeleton_df(df,measure):
-    
-    date_columns = ['Date','Day','OrdDay']
-    
-    rev_df_columns = ['SiteName', 'LocationName', 'GenericLocation', 'Session', 'OrdSession', 'RevenueType', 'Wine', 'Date', 'Day', 'OrdDay', 'Revenue']
-    cov_df_columns = ['SiteName', 'LocationName', 'GenericLocation', 'Session', 'Date', 'Day', 'OrdDay', 'Covers']    
-    df_columns = rev_df_columns if measure=='Revenue' else cov_df_columns
-
-    skeleton_df_columns = df_columns.copy()
-    for col in [measure] + date_columns:
-        skeleton_df_columns.remove(col)
-
-    skeleton_df = df[skeleton_df_columns].groupby(skeleton_df_columns).sum().reset_index()
-
-    if measure == 'Revenue':
-
-        rows = [
-            [
-                row[0], 
-                row[1], 
-                row[2], 
-                row[3], 
-                row[4], 
-                row[5], 
-                row[6]
-            ] for row in zip(
-                skeleton_df[skeleton_df_columns[0]],
-                skeleton_df[skeleton_df_columns[1]],
-                skeleton_df[skeleton_df_columns[2]],
-                skeleton_df[skeleton_df_columns[3]],
-                skeleton_df[skeleton_df_columns[4]],
-                skeleton_df[skeleton_df_columns[5]],
-                skeleton_df[skeleton_df_columns[6]]
-        )]
-
-    else:
-
-        rows = [
-            [
-                row[0], 
-                row[1], 
-                row[2], 
-                row[3], 
-            ] for row in zip(
-                skeleton_df[skeleton_df_columns[0]],
-                skeleton_df[skeleton_df_columns[1]],
-                skeleton_df[skeleton_df_columns[2]],
-                skeleton_df[skeleton_df_columns[3]],
-        )]
-
-    skeleton_dates = df[date_columns].groupby(date_columns).sum().reset_index()
-
-    date_rows = [
-        [
-            row[0], 
-            row[1], 
-            row[2]
-        ] for row in zip(
-            skeleton_dates[date_columns[0]],
-            skeleton_dates[date_columns[1]],
-            skeleton_dates[date_columns[2]]
-    )]
-
-    skeleton = pd.DataFrame([row + date_row for row in rows for date_row in date_rows])
-    skeleton.columns = df_columns.copy()[:-1]
-
-    return pd.merge(skeleton,df,how='outer').fillna(0)
-
-# rev_df = generate_skeleton_df(revenue.dataframe, 'Revenue')
-# cov_df = generate_skeleton_df(covers.dataframe, 'Covers')
+rev_df = generate_skeleton_df(revenue.dataframe, 'Revenue')
+cov_df = generate_skeleton_df(covers.dataframe, 'Covers')
 
 # Add Date Columns
 rev_df['Date'] = pd.to_datetime(rev_df['Date']).dt.date
@@ -106,32 +44,3 @@ weekly_rev_df = bound_filtering(rev_df,weekly_bounds)
 weekly_cov_df = bound_filtering(cov_df,weekly_bounds)
 monthly_rev_df = bound_filtering(rev_df,monthly_bounds)
 monthly_cov_df = bound_filtering(cov_df,monthly_bounds)
-
-# tracker_df = tracker.dataframe
-# pickup_df = pickup.dataframe
-# trends_df = trends.dataframe
-# future_df = future.dataframe
-
-# future_df['weekday'] = pd.to_datetime(future_df['visit_day']).dt.weekday_name
-
-# Function to ensure any infinite values are converted to 0 to allow calculation
-
-# def infinite(x): 
-#     if x == float('inf'): 
-#         return 0 
-#     elif x == float('-inf'): 
-#         return 0 
-#     else: 
-#         return x
-    
-
-# rev_df = pd.read_csv("data/App Revenue.csv")
-# cov_df = pd.read_csv("data/App Covers.csv")
-
-# tracker_df = pd.read_csv('data/Tracker.csv').iloc[:,1:]
-# pickup_df = pd.read_csv('data/Pickup.csv').iloc[:,1:]
-
-# future_df = pd.read_csv("data/Future Bookings.csv")
-# future_df['weekday'] = pd.to_datetime(future_df['visit_day']).dt.weekday_name
-
-# trends_df = pd.read_csv('data/Booking Trends.csv')
