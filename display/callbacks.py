4946659ec5dc1f73a9ba4414de94b597c40250fe
domgunewardena <66011295+domgunewardena@users.noreply.github.com>
2021-01-07 14:50:48 +0000

    Update callbacks.py

--- display/callbacks.py
+++ display/callbacks.py
@@ -7,7 +7,48 @@ from data.date_bounds import *
 from data.day_counts import *
 from app import app
 
-# Daily Sales Callbacks
+import dash
+
+# Callback Inputs
+
+def dropdown_dependencies(report):
+    
+    def dropdown_ids(report):
+        dropdowns = ['shift','area','measure','metric','site']
+        return [report + ' ' + dropdown + ' dropdown' for dropdown in dropdowns]
+    
+    dropdown_ids = dropdown_ids(report)
+    return [dash.dependencies.Input(x, 'value') for x in dropdown_ids]
+
+def week_dropdown_dependencies(report):
+    
+    def week_dropdown_ids(report):
+        dropdowns = ['site','area','category','measure']
+        return [report + ' ' + dropdown + ' week dropdown' for dropdown in dropdowns] + [report + ' metric dropdown'] + [report + ' metric week dropdown']
+
+    dropdown_ids = week_dropdown_ids(report)
+    return [dash.dependencies.Input(x, 'value') for x in dropdown_ids]
+
+daily_dropdown_dependencies = dropdown_dependencies('daily')
+wtd_dropdown_dependencies = dropdown_dependencies('wtd')
+mtd_dropdown_dependencies = dropdown_dependencies('mtd')
+weekly_dropdown_dependencies = dropdown_dependencies('weekly')
+monthly_dropdown_dependencies = dropdown_dependencies('monthly')
+
+wtd_week_dropdown_dependencies = week_dropdown_dependencies('wtd')
+mtd_week_dropdown_dependencies = week_dropdown_dependencies('mtd')
+weekly_week_dropdown_dependencies = week_dropdown_dependencies('weekly')
+monthly_week_dropdown_dependencies = week_dropdown_dependencies('monthly')
+
+tracker_dropdown_ids = ['tracker_week_dropdown','tracker_metric_dropdown','tracker_site_dropdown']
+tracker_dropdown_dependencies = [dash.dependencies.Input(x, 'value') for x in tracker_dropdown_ids]
+
+pickup_dropdown_ids = ['pickup_week_dropdown','pickup_metric_dropdown','pickup_site_dropdown']
+pickup_dropdown_dependencies = [dash.dependencies.Input(x, 'value') for x in pickup_dropdown_ids]
+
+# Callbacks
+
+# Daily Sales
 
 @app.callback(dash.dependencies.Output('daily site dropdown', 'options'),
               [dash.dependencies.Input('daily shift dropdown', 'options')])
@@ -46,7 +87,6 @@ def update_daily_site_covers(shift,area,measure,metric,site):
 @app.callback(dash.dependencies.Output('daily site spend', 'figure'),daily_dropdown_dependencies)
 def update_daily_site_spend(shift,area,measure,metric,site):
     return sales_site_spend_graph(shift,metric,site,daily_rev_df,daily_cov_df,daily_bounds,daily_current_col,daily_last_col,daily_vs_col)
-    
     
 # WTD
 
