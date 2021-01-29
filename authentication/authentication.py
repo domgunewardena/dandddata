151f1d3616f94031f50640c07bcea5f19d2c40d9
domgunewardena <66011295+domgunewardena@users.noreply.github.com>
2021-01-29 12:36:59 +0000

    Take passwords from config vars

--- authentication/authentication.py
+++ authentication/authentication.py
@@ -1,11 +1,14 @@
 from authentication import dash_auth
 from app import app
 
-VALID_USERNAME_PASSWORD_PAIRS = {
-    'dandd':'london123',
-    'michaelf':'london123',
-    'sharon':'london123',
-    'jb':'london123'
+import os
+app_password = os.environ['GENERAL_PASSWORD']
+
+password_pairs = {
+  'dandd':app_password,
+  'michaelf':app_password,
+  'sharon':app_password,
+  'jb':app_password,
 }
 
-auth = dash_auth.BasicAuth(app,VALID_USERNAME_PASSWORD_PAIRS)
+auth = dash_auth.BasicAuth(app,password_pairs)
