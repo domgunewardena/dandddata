0f58a21689367534e44951d637b206b208411677
domgunewardena <66011295+domgunewardena@users.noreply.github.com>
2021-01-29 12:37:14 +0000

    Delete passwords.py

--- authentication/passwords.py
+++
@@ -1,9 +0,0 @@
-import os
-app_password = os.environ['GENERAL_PASSWORD']
-
-password_pairs = {
-  'dandd':app_password,
-  'michaelf':app_password,
-  'sharon':app_password,
-  'jb':app_password,
-}
