cca73ef3116983a2686db5519d6fabaec227b04d
domgunewardena <66011295+domgunewardena@users.noreply.github.com>
2021-01-29 12:29:01 +0000

    Create passwords.py

---
+++ authentication/passwords.py
@@ -0,0 +1,9 @@
+import os
+app_password = os.environ['GENERAL_PASSWORD']
+
+password_pairs = {
+  'dandd':app_password,
+  'michaelf':app_password,
+  'sharon':app_password,
+  'jb':app_password,
+}
