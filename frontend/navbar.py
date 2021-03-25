ee699c6a56a59df0e17c4e1e897d2ad2a0fb88b5
domgunewardena <domgunewardena@gmail.com>
2021-03-25 00:55:00 +0000

    Expand sales and bookings dropdowns on sidebar

--- frontend/navbar.py
+++ frontend/navbar.py
@@ -102,64 +102,74 @@ sidebar = html.Div(
                 dbc.NavItem(dbc.NavLink("Group", href="/", active="exact")),
                 dbc.NavItem(dbc.NavLink("Breakdown", href="/breakdown", active="exact")),
                 dbc.NavItem(dbc.NavLink("Restaurant", href="/restaurant", active="exact")),
-                dbc.DropdownMenu(
-                    children=[
-                        dbc.DropdownMenuItem(
-                            'Sales Reports', 
-                            header=True
-                        ),
-                        dbc.DropdownMenuItem(
-                            'Daily Sales', 
-                            href="/daily-sales"
-                        ),
-                        dbc.DropdownMenuItem(
-                            'WTD Sales', 
-                            href="/wtd-sales"
-                        ),
-                        dbc.DropdownMenuItem(
-                            'MTD Sales', 
-                            href="/mtd-sales"
-                        ),
-                        dbc.DropdownMenuItem(
-                            'Weekly Sales', 
-                            href="/weekly-sales"
-                        ),
-                        dbc.DropdownMenuItem(
-                            'Monthly Sales', 
-                            href="/monthly-sales"
-                        ),
-                    ],
-                    nav=True,
-                    in_navbar=True,
-                    label='Sales',
-                ),
-                dbc.DropdownMenu(
-                    children=[
-                        dbc.DropdownMenuItem(
-                            'Bookings Reports', 
-                            header=True
-                        ),
-                        dbc.DropdownMenuItem(
-                            'Cover Tracker', 
-                            href="/tracker"
-                        ),
-                        dbc.DropdownMenuItem(
-                            'Daily Pickup', 
-                            href="/pickup"
-                        ),
-                        dbc.DropdownMenuItem(
-                            'Future Bookings', 
-                            href="/future"
-                        ),
-                        dbc.DropdownMenuItem(
-                            'Booking Trends', 
-                            href="/trends"
-                        ),
-                    ],
-                    nav=True,
-                    in_navbar=True,
-                    label='Bookings',
-                ),
+                html.Hr(),
+                dbc.NavItem(dbc.NavLink('Daily Sales', href="/daily-sales", active="exact")),
+                dbc.NavItem(dbc.NavLink('WTD Sales', href="/wtd-sales", active="exact")),
+                dbc.NavItem(dbc.NavLink('MTD Sales', href="/mtd-sales", active="exact")),
+                dbc.NavItem(dbc.NavLink('Weekly Sales', href="/weekly-sales", active="exact")),
+                dbc.NavItem(dbc.NavLink('Monthly Sales', href="/monthly-sales", active="exact")),
+                html.Hr(),
+                dbc.NavItem(dbc.NavLink('Cover Tracker', href="/tracker", active="exact")),
+                dbc.NavItem(dbc.NavLink('Daily Pickup', href="/pickup", active="exact")),
+                dbc.NavItem(dbc.NavLink('Future Bookings', href="/future", active="exact")),
+#                 dbc.DropdownMenu(
+#                     children=[
+#                         dbc.DropdownMenuItem(
+#                             'Sales Reports', 
+#                             header=True
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'Daily Sales', 
+#                             href="/daily-sales"
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'WTD Sales', 
+#                             href="/wtd-sales"
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'MTD Sales', 
+#                             href="/mtd-sales"
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'Weekly Sales', 
+#                             href="/weekly-sales"
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'Monthly Sales', 
+#                             href="/monthly-sales"
+#                         ),
+#                     ],
+#                     nav=True,
+#                     in_navbar=True,
+#                     label='Sales',
+#                 ),
+#                 dbc.DropdownMenu(
+#                     children=[
+#                         dbc.DropdownMenuItem(
+#                             'Bookings Reports', 
+#                             header=True
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'Cover Tracker', 
+#                             href="/tracker"
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'Daily Pickup', 
+#                             href="/pickup"
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'Future Bookings', 
+#                             href="/future"
+#                         ),
+#                         dbc.DropdownMenuItem(
+#                             'Booking Trends', 
+#                             href="/trends"
+#                         ),
+#                     ],
+#                     nav=True,
+#                     in_navbar=True,
+#                     label='Bookings',
+#                 ),
             ],
             vertical=True,
             pills=True,
